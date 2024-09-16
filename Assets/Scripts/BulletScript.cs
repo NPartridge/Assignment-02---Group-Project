d1af84f8423e816f4c63a7d75732b6f159056025
NPartridge <60328660+NPartridge@users.noreply.github.com>
2024-09-17 11:21:35 +1200

    Destroy enemy and bullet
    - Increase enemy prefab radius to 1 to make it easier for bullet to hit
    - on enemy hit, destroy enemy and bullet
    - destroy bullet when it goes beyond its max range

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class BulletScript : MonoBehaviour
 {
     [SerializeField] float speed = 10f;
+    [SerializeField] float maxDistance = 20f;
     void Start()
     {
 
@@ -11,5 +12,23 @@ public class BulletScript : MonoBehaviour
     void Update()
     {
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        // Destroy bullet when it goes beyond its max range
+        float distanceTravelled = transform.position.magnitude;
+        if (distanceTravelled > maxDistance)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            // Destroy the enemy on hit
+            Destroy(other.gameObject);
+            // Destroy the bullet on hit
+            Destroy(gameObject);
+
+        }
     }
 }
