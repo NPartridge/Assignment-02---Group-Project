2d426d5078a051fd2d505b5cbfd75f3dfcedb37e
NPartridge <60328660+NPartridge@users.noreply.github.com>
2024-09-17 11:46:15 +1200

    Fix bug in bullet destroy

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -4,17 +4,28 @@ public class BulletScript : MonoBehaviour
 {
     [SerializeField] float speed = 10f;
     [SerializeField] float maxDistance = 20f;
+
+    Vector3 startPosition;
+
     void Start()
     {
-
+        startPosition = transform.position;
     }
 
     void Update()
     {
+        BulletMovement();
+    }
+
+    private void BulletMovement()
+    {
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
         // Destroy bullet when it goes beyond its max range
-        float distanceTravelled = transform.position.magnitude;
-        if (distanceTravelled > maxDistance)
+        Vector3 bulletDisplacement = transform.position - startPosition;
+        float bulletDistance = bulletDisplacement.magnitude;
+
+        if (bulletDistance > maxDistance)
         {
             Destroy(gameObject);
         }
