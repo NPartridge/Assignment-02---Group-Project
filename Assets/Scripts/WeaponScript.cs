c497abe294c3b67bd4593d7bd332f5efb75daf66
NPartridge <60328660+NPartridge@users.noreply.github.com>
2024-09-17 11:57:03 +1200

    Create a weapon timer

--- Assets/Scripts/WeaponScript.cs
+++ Assets/Scripts/WeaponScript.cs
@@ -1,13 +1,20 @@
 using UnityEngine;
 
+// Code for timer from: https://docs.unity3d.com/ScriptReference/Time-time.html
+
 public class WeaponScript : MonoBehaviour
 {
+    [SerializeField] float fireRate = 0.5f;
+    private float nextFire = 0.0f;
+
     public GameObject bulletPrefab;
+    
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && Time.time > nextFire)
         {
+            nextFire = Time.time + fireRate;
             Shoot();
         }
     }
