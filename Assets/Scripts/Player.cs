3389e86d32f1f72678e746c4194c60aa8950da79
NPartridge <60328660+NPartridge@users.noreply.github.com>
2024-09-17 11:58:54 +1200

    Add reference for info on rotate to mouse position

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 
+// Information to help rotate to mouse position found here: https://discussions.unity.com/t/rotate-towards-mouse-position/883950
 public class Player : MonoBehaviour
 {
     [Header("Player stat sheet")]
@@ -30,8 +31,10 @@ public class Player : MonoBehaviour
     private void RotatePlayer()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
         if (Physics.Raycast(ray, out RaycastHit hit))
         {
+            // Only interested in position of mouse on x,z axes, keep player rotation on y axis
             transform.LookAt(new Vector3(hit.point.x, transform.position.y, hit.point.z));
         }
     }
