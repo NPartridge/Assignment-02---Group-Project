6d7f5e5aa61066be13df973ed78b7d4dc941ab05
NPartridge <60328660+NPartridge@users.noreply.github.com>
2024-09-30 21:46:55 +1300

    Create a crate script that spawns a random object on collision

---
+++ Assets/Scripts/CrateScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrateScript : MonoBehaviour
+{
+    // List of items dropped from a crate
+    public List<GameObject> dropTable;
+
+    private void OnTriggerEnter(Collider other)
+    {   
+        if (other.CompareTag("Bullet")){
+            // spawn an item
+            SpawnRandomDrop();
+            // destroy the bullet
+            Destroy(other.gameObject);
+            // destroy the box
+            Destroy(gameObject);
+        }
+    }
+
+    void SpawnRandomDrop()
+    {
+        // chose a random item from the droptable for a crate
+        int itemNumber = Random.Range(0, dropTable.Count);
+        // spawn the drop item
+        Instantiate(dropTable[0], transform.position, Quaternion.identity);
+    }
+}
