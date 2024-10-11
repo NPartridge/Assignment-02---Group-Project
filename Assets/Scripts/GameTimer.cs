1427f5f2ac086b48f40baf5876b573b65b548388
owlbeach <reecebonnington1@gmail.com>
2024-10-12 11:50:26 +1300

    Game over screen fixes
    Added text fields to the game over screen to display how long the player survived, experience collected, and enemies killed.
    Updated the in game timer so it now gets disabled when the player dies.

--- Assets/Scripts/GameTimer.cs
+++ Assets/Scripts/GameTimer.cs
@@ -22,6 +22,16 @@ public class GameTimer : MonoBehaviour
         }
     }
     
+    public void HideTimerUI()
+    {
+        timerText.gameObject.SetActive(false);
+    }
+    
+    public void ShowTimerUI()
+    {
+        timerText.gameObject.SetActive(true);
+    }
+    
     // This functions code comes from here. I didn't watch the video at all... just skipped to the end to see the code
     // How to make a Countdown Timer in Unity (in minutes + seconds) https://www.youtube.com/watch?v=HmHPJL-OcQE
     private void UpdateTimerUI()
@@ -31,5 +41,18 @@ public class GameTimer : MonoBehaviour
         int milliseconds = Mathf.FloorToInt((elapsedTime % 1) * 1000);
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
         //timerText.text = string.Format("{0:00}:{1:00}:{2:00}", minutes, seconds, milliseconds); // Can use this if we want to include milliseconds
+    }
+    
+    public void ResetTimer()
+    {
+        elapsedTime = 0f;
+        UpdateTimerUI();
+        
+        ShowTimerUI();
+    }
+    
+    public float GetElapsedTime()
+    {
+        return elapsedTime;
     }
 }
