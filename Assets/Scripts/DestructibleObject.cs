90006b5dc22e5110dff985d3eeda84e6174b93b8
owlbeach <reecebonnington1@gmail.com>
2024-10-20 17:14:06 +1300

    Barrel spawning system
    Deleted old nav meshes.
    Changed the script name from CrateScript to DestructibleObjects.
    Added a folder named Destructibles and added the barrels into that.
    Added destructible spawning system which currently respawn based on a static cooldown. This could be changed so each object has its own cooldown.
    Added a 'range' variable to the player so they can only shoot at barrels that are within range.
    Placed a bunch of barrels around the scene.

---
+++ Assets/Scripts/DestructibleObject.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DestructibleObject : MonoBehaviour
+{
+    // List of items dropped from the object
+    public List<GameObject> dropTable;
+    
+    private MeshRenderer[] meshRenderers;
+    private Collider[] colliders;
+    private NavMeshObstacle navObstacle;
+    
+    public bool IsActive { get; private set; }
+
+    private void Start()
+    {
+        // We need the mesh renderers and colliders for respawning the objects. To respawn the objects we will
+        // just disable and re-enable the renderers/colliders 
+        meshRenderers = GetComponentsInChildren<MeshRenderer>();
+        colliders = GetComponentsInChildren<Collider>();
+        navObstacle = GetComponent<NavMeshObstacle>();
+        
+        IsActive = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Bullet"))
+        {
+            // spawn an item
+            SpawnRandomDrop();
+            // destroy the bullet
+            Destroy(other.gameObject);
+            // destroy the object
+            StartCoroutine(DeactivateAndRespawn());
+        }
+    }
+
+    void SpawnRandomDrop()
+    {
+        // chose a random item from the droptable for the object
+        int itemNumber = Random.Range(0, dropTable.Count);
+        // spawn the drop item at space 1 unit above the object location
+        Instantiate(dropTable[itemNumber], transform.position + Vector3.up, Quaternion.identity);
+    }
+    
+    private IEnumerator DeactivateAndRespawn()
+    {
+        // The object has been destroyed at this point so we are disabling everything in this section
+        IsActive = false;
+        
+        foreach (var renderer in meshRenderers)
+        {
+            renderer.enabled = false;
+        }
+        
+        foreach (var col in colliders)
+        {
+            col.enabled = false;
+        }
+        
+        if (navObstacle != null)
+        {
+            navObstacle.enabled = false;
+        }
+        
+        // The spawn timer for the object. We re-enable everything after this point as the object should respawn
+        yield return new WaitForSeconds(10f);
+        
+        IsActive = true;
+        
+        foreach (var renderer in meshRenderers)
+        {
+            renderer.enabled = true;
+        }
+        
+        foreach (var col in colliders)
+        {
+            col.enabled = true;
+        }
+        
+        if (navObstacle != null)
+        {
+            navObstacle.enabled = true;
+        }
+    }
+}
