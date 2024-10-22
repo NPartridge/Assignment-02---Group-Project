5de13605da0c3c284cfb7ca8c356f57d1a93fef3
owlbeach <reecebonnington1@gmail.com>
2024-10-23 10:04:29 +1300

    Experience bar/level counter and speed potion fix
    Speed buff icon displays bottom middle, player can't pick up multiple speed buffs at the same time. If the player picks up a speed potion whilst one is active then the duration is reset.
    
    Player has an experience bar showing the amount of experience they have and the xp needed to reach the next level. Their level is also displayed ontop of the bar.

--- Assets/Scripts/SpeedPotionScript.cs
+++ Assets/Scripts/SpeedPotionScript.cs
@@ -3,8 +3,6 @@ using UnityEngine;
 
 public class SpeedPotionScript : MonoBehaviour
 {
-    [SerializeField] SoundEffect soundEffect;
-    AudioSource audioSource;
     public float speedIncreaseValue = 2.5f;
     public float duration = 5f;
 
@@ -12,7 +10,6 @@ public class SpeedPotionScript : MonoBehaviour
     private void Start()
     {
         bounceClass = GetComponent<BounceClass>();
-        audioSource = GetComponent<AudioSource>();
     }
 
     private void Update()
@@ -24,12 +21,11 @@ public class SpeedPotionScript : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            audioSource.PlayOneShot(soundEffect.SpeedPotion);
             // temporarily increase player speed
             Player player = other.GetComponent<Player>();
-            player.StartCoroutine(player.ApplyTemporarySpeedBuff(speedIncreaseValue, duration));
+            player.ApplyTemporarySpeedBuff(speedIncreaseValue, duration);
             // destroy the potion
-            Destroy(gameObject, 0.5f);
+            Destroy(gameObject);
         }
     }
 }
